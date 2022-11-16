4116a0f9c5f15ba14de93511a32dbb8dbef39817
Maurilio oliver <71782456+maurilio-oliver@users.noreply.github.com>
2022-11-16 15:14:14 -0300

    first commit

---
+++ src/test/java/com/makeplace/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeplace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
